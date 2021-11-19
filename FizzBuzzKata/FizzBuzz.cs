f4224ce66d96553ed62830b3a056a2802b0288ac
salvarez <salvarez@voxelgroup.net>
2021-11-19 12:43:20 +0100

    Refactor: Removed GetOneToOneHundred method and tests

--- FizzBuzzKata/FizzBuzz.cs
+++ FizzBuzzKata/FizzBuzz.cs
@@ -4,18 +4,6 @@ namespace FizzBuzzKata
 {
     public class FizzBuzz
     {
-        public static int[] GetOneToOneHundred()
-        {
-            var intArray = new int[100];
-
-            for (var i = 0; i < 100; i++)
-            {
-                intArray[i] = i+1;
-            }
-
-            return intArray;
-        }
-
         public static bool IsFizzAndBuzz(int input)
         {
             if (IsFizz(input) && IsBuzz(input))

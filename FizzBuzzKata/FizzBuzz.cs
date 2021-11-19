5d93c382ae69f33b79ec1ed32e0ab9ce289bd360
salvarez <salvarez@voxelgroup.net>
2021-11-19 10:35:26 +0100

    ReturnsThreeInPositionTwo

--- FizzBuzzKata/FizzBuzz.cs
+++ FizzBuzzKata/FizzBuzz.cs
@@ -6,10 +6,11 @@ namespace FizzBuzzKata
     {
         public static int[] GetOneToOneHundred()
         {
-            var intArray = new int[2];
+            var intArray = new int[3];
 
             intArray[0] = 1;
             intArray[1] = 2;
+            intArray[2] = 3;
 
             return intArray;
         }

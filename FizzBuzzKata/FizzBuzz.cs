b678a0d0dffb5d7fe7f01e1d05a7e5c4d4b7be38
salvarez <salvarez@voxelgroup.net>
2021-11-19 13:04:20 +0100

    FizzBuzzMain_ReturnFizzForInputFifteen

--- FizzBuzzKata/FizzBuzz.cs
+++ FizzBuzzKata/FizzBuzz.cs
@@ -4,16 +4,11 @@ namespace FizzBuzzKata
 {
     public class FizzBuzz
     {
-        public static bool IsFizzAndBuzz(int input)
-        {
-            if (IsFizz(input) && IsBuzz(input))
-                return true;
-
-            return false;
-        }
-
         public static string FizzBuzzMain(int v)
         {
+            if (IsFizz(v) && IsBuzz(v))
+                return "FizzBuzz";
+
             if (IsBuzz(v))
                 return "Buzz";
 
@@ -34,6 +29,14 @@ namespace FizzBuzzKata
         public static bool IsBuzz(int input)
         {
             if (input % 5 == 0)
+                return true;
+
+            return false;
+        }
+
+        public static bool IsFizzAndBuzz(int input)
+        {
+            if (IsFizz(input) && IsBuzz(input))
                 return true;
 
             return false;
